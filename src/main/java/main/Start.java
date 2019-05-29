08f3c8c2ee7c0d8acc550c08395f60322d5c4d44
Kiryl Padhaiski <46563159+KirylPod@users.noreply.github.com>
2019-05-29 19:48:05 +0300

    continue

--- src/main/java/main/Start.java
+++ src/main/java/main/Start.java
@@ -1,4 +1,19 @@
 package main;
 
+import impls.car.CreateCar;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Start {
+
+    public static void main (String[] args){
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("car_context.xml");
+        CreateCar createCar=(CreateCar)context.getBean("createCar");
+        createCar.go();
+
+
+    }
+
+
 }
